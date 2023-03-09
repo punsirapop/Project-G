ea68193cae7e0ea5c71e6447bb9ccb1f6ee95ea0
Thanapat Mahiskhamin <gun2870@gmail.com>
2023-03-09 21:02:03 +0700

    Create functional Roulette Wheel

--- Assets/Scripts/SelectionPuzzle/CandidateManager.cs
+++ Assets/Scripts/SelectionPuzzle/CandidateManager.cs
@@ -28,7 +28,8 @@ public class CandidateManager : MonoBehaviour
     // RB: {Rank, ReverseFitness, Chance, Wheel, SpinWheel, SpinWheel, SpinWheel, SpinWheel, SpinWheel, SpinWheel}
     [SerializeField] private List<Operation> _OperationLog;
     public List<Operation> OperationLog => _OperationLog;
-    [SerializeField] private Transform _ChromosomeHolder;
+    [SerializeField] private GameObject _ChromosomeHolder;
+    [SerializeField] private GameObject _RouletteWheel;
     
     void Awake()
     {
@@ -38,7 +39,7 @@ public class CandidateManager : MonoBehaviour
     void Start()
     {
         // Destroy all object in this panel (if any)
-        foreach (Transform child in _ChromosomeHolder)
+        foreach (Transform child in _ChromosomeHolder.transform)
         {
             Destroy(child.gameObject);
         }
@@ -47,8 +48,10 @@ public class CandidateManager : MonoBehaviour
     // Copy the number of chromosome from the population equal to the given candidateCount
     public void CreateGroup(int candidateCount)
     {
+        _ChromosomeHolder.SetActive(true);
+        _RouletteWheel.SetActive(false);
         // Destroy all object in this panel (if any)
-        foreach (Transform child in _ChromosomeHolder)
+        foreach (Transform child in _ChromosomeHolder.transform)
         {
             Destroy(child.gameObject);
         }
@@ -67,10 +70,9 @@ public class CandidateManager : MonoBehaviour
             {
                 int randomIndex = Random.Range(0, populationPool.Length);
                 // Instantiate the actual GameObject of the candidate
-                GameObject newIidividual = Instantiate(populationPool[randomIndex], _ChromosomeHolder);
-                newIidividual.AddComponent<Button>();
-                ChromosomeRod newIndividualRod = newIidividual.GetComponentInChildren<ChromosomeRod>();
-                newIidividual.GetComponent<Button>().onClick.AddListener(() => SelectedParentManager.Instance.AddSelectedChromosome(newIndividualRod));
+                GameObject newIndividual = Instantiate(populationPool[randomIndex], _ChromosomeHolder.transform);
+                ChromosomeRod newIndividualRod = newIndividual.GetComponentInChildren<ChromosomeRod>();
+                newIndividual.AddComponent<Button>().onClick.AddListener(() => SelectedParentManager.Instance.AddSelectedChromosome(newIndividualRod));
                 // Remove selected index from the pool
                 GameObject[] newPool = new GameObject[populationPool.Length - 1];
                 for (int i = 0; i < newPool.Length; i++)
@@ -82,5 +84,49 @@ public class CandidateManager : MonoBehaviour
         }
         // Record the operation
         _OperationLog.Add(Operation.Group);
+    }
+
+    // Create the chance to be selected of each chromosome
+    public void CreateChance()
+    {
+        _ChromosomeHolder.SetActive(true);
+        _RouletteWheel.SetActive(false);
+        // Destroy all object in this panel (if any)
+        foreach (Transform child in _ChromosomeHolder.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        // If the given number of candidate exceed the number of population, do nothing
+        GameObject[] population = PopulationManager.Instance.Population;
+        // Calculate total fitness
+        int totalFitness = 0;
+        foreach (GameObject individual in population)
+        {
+            totalFitness += individual.GetComponentInChildren<ChromosomeRodValue>().Value;
+        }
+        // Calculate the actual GameObject with the proportion percentage
+        foreach (GameObject individual in population)
+        {
+            GameObject newIndividual = Instantiate(individual, _ChromosomeHolder.transform);
+            ChromosomeRod newIndividualRod = newIndividual.GetComponentInChildren<ChromosomeRod>();
+            ChromosomeRodValue newIndividualRodValue = newIndividual.GetComponent<ChromosomeRodValue>();
+            newIndividual.AddComponent<Button>().onClick.AddListener(() => SelectedParentManager.Instance.AddSelectedChromosome(newIndividualRod));
+            float percentage = (float)newIndividualRodValue.Value / (float)totalFitness * 100f;
+            newIndividualRodValue.SetValue(percentage);
+        }
+    }
+
+    // Create Roulette Wheel using the fitness of each individual in population
+    public void CreateWheel()
+    {
+        ChromosomeRodValue[] currentRodValues = GetComponentsInChildren<ChromosomeRodValue>();
+        _ChromosomeHolder.SetActive(false);
+        _RouletteWheel.SetActive(true);
+        _RouletteWheel.GetComponent<RouletteWheel>().SetWheel(currentRodValues);
+    }
+
+    public void AddParentFromWheel(int index)
+    {
+        SelectedParentManager.Instance.AddSelectedChromosome(GetComponentsInChildren<ChromosomeRod>(true)[index]);
     }
 }
--- Assets/Scripts/SelectionPuzzle/ChromosomeRodValue.cs
+++ Assets/Scripts/SelectionPuzzle/ChromosomeRodValue.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class ChromosomeRodValue : MonoBehaviour
 {
+    [SerializeField] public Color32 Color => GetComponent<ChromosomeRod>().GetColorAtIndex(1);
     [SerializeField] private int _Value;
+    [SerializeField] public int Value => _Value;
+    [SerializeField] private float _Percentage;
     [SerializeField] private bool _IsPercentage;
     [SerializeField] private TextMeshProUGUI _FitnessText;
 
-    public void SetValue(int newValue, bool newIsPercentage = false)
+    // Set fitness value
+    public void SetValue(int newValue)
     {
         _Value = newValue;
-        _IsPercentage = newIsPercentage;
-        string valueText = _Value.ToString();
-        valueText += _IsPercentage ? "%" : "";
-        _FitnessText.text = valueText;
+        _IsPercentage = false;
+        _FitnessText.text = _Value.ToString();
+    }
+
+    // Overloading function for set percentage value
+    public void SetValue(float newValue)
+    {
+        _Percentage = newValue;
+        _IsPercentage = true;
+        _FitnessText.text = ((int)_Percentage).ToString() + "%";
     }
 }
