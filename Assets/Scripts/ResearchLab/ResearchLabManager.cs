28c0951ba0f4c15bbe3519d1e234b0306be236eb
Thanapat Mahiskhamin <gun2870@gmail.com>
2023-03-30 16:28:09 +0700

    Properly set buttons in the ResearchLab
    - Previous/next page button is interactable only if there is available previous/next page
    - Add all 3 chapters about knapsack problem
    - Highlight the important word(s) in description of each content page using bold text formatting

--- Assets/Scripts/ResearchLab/ResearchLabManager.cs
+++ Assets/Scripts/ResearchLab/ResearchLabManager.cs
@@ -9,25 +9,31 @@ public class ResearchLabManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _ContentPageHeader;
     [SerializeField] private Image _ContentPageImage;
     [SerializeField] private TextMeshProUGUI _ContentPageDescription;
+    // Button object for colorize purpose
+    [SerializeField] private GameObject _PreviousPageButton;
+    [SerializeField] private GameObject _NextPageButton;
+    [SerializeField] private GameObject[] _TabButtons;
+    [SerializeField] private GameObject[] _ChapterButtons;
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
     [SerializeField] private ContentChapterSO[] Chapters;
     private int _CurrentChapter;
     private int _CurrentPage;
 
-    private void Awake()
+    private void Start()
     {
-        _CurrentChapter = 0;
-        _CurrentPage = 0;
+        ChangeChapter(0);
     }
 
     private void Update()
     {
+        // Continually display the content of the current page
         _ContentPageHeader.text = Chapters[_CurrentChapter].Contents[_CurrentPage].Header;
         _ContentPageImage.sprite = Chapters[_CurrentChapter].Contents[_CurrentPage].Image;
         _ContentPageDescription.text = Chapters[_CurrentChapter].Contents[_CurrentPage].Description;
     }
 
+    // Add the page of current chapter
     public void AddPage(int amount)
     {
         _CurrentPage += amount;
@@ -39,11 +45,42 @@ public class ResearchLabManager : MonoBehaviour
         {
             _CurrentPage = Chapters[_CurrentChapter].Contents.Length - 1;
         }
+        _SetPageButtons();
     }
 
+    // Change the content chapter
     public void ChangeChapter(int newChapter)
     {
+        Debug.Log("Change to chapter " + newChapter.ToString());
         _CurrentChapter = newChapter;
         _CurrentPage = 0;
+        _SetChapterButtons();
+        _SetPageButtons();
+    }
+
+    // Set change page button to proper color
+    private void _SetPageButtons()
+    {
+        int maxPage = Chapters[_CurrentChapter].Contents.Length - 1;
+        _PreviousPageButton.GetComponent<Image>().color = (_CurrentPage > 0) ? Color.green : Color.white;
+        _PreviousPageButton.GetComponent<Button>().interactable = (_CurrentPage > 0) ? true : false;
+        _NextPageButton.GetComponent<Image>().color = (_CurrentPage < maxPage) ? Color.green : Color.white;
+        _NextPageButton.GetComponent<Button>().interactable = (_CurrentPage < maxPage) ? true : false;
+    }
+    // Set chapter button to proper color
+    private void _SetChapterButtons()
+    {
+        Debug.Log("ChapterButtons length = " + _ChapterButtons.Length.ToString());
+        Debug.Log("Current chapter = " + _CurrentChapter.ToString());
+        for (int i = 0; i < _ChapterButtons.Length; i++)
+        {
+            _ChapterButtons[i].GetComponent<Button>().interactable = (i == _CurrentChapter) ? false : true;
+            if (i == _CurrentChapter)
+            {
+                Debug.Log("Button of Chapter " + i.ToString() + " not interactable");
+                Debug.Log(_ChapterButtons[i].GetComponent<Button>().interactable);
+            }
+        }
+        Debug.Log("Set ChapterButtons");
     }
 }
