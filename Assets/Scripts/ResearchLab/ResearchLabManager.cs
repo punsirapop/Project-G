eb61e886a53457675ae8e0fa796367e3fbacbefe
Thanapat Mahiskhamin <gun2870@gmail.com>
2023-03-31 17:04:24 +0700

    Create ChapterButton on ResearchLab
    - Spawn ChapterButton according to ContentChapterSO in ResearchLabManager
    - Add all remaining ContentChapterSO except the Flow of Genetic Algorithm which is needed to be revised first

--- Assets/Scripts/ResearchLab/ResearchLabManager.cs
+++ Assets/Scripts/ResearchLab/ResearchLabManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class ResearchLabManager : MonoBehaviour
 {
+    public static ResearchLabManager Instance;
+
+    [SerializeField] private GameObject _ChapterButtonPrefab;
+    [SerializeField] private Transform _ChapterButtonHolder;
+    // UI element for displaying one page of content
     [SerializeField] private TextMeshProUGUI _ContentPageHeader;
     [SerializeField] private Image _ContentPageImage;
     [SerializeField] private TextMeshProUGUI _ContentPageDescription;
@@ -13,24 +18,42 @@ public class ResearchLabManager : MonoBehaviour
     [SerializeField] private GameObject _PreviousPageButton;
     [SerializeField] private GameObject _NextPageButton;
     [SerializeField] private GameObject[] _TabButtons;
-    [SerializeField] private GameObject[] _ChapterButtons;
+    [SerializeField] private ChapterButton[] _ChapterButtons;
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-    [SerializeField] private ContentChapterSO[] Chapters;
-    private int _CurrentChapter;
+    [SerializeField] private ContentChapterSO[] _Chapters;
+    private ContentChapterSO _CurrentChapterSO;
     private int _CurrentPage;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
 
     private void Start()
     {
-        ChangeChapter(0);
+        // Destory all previous child object
+        foreach (Transform child in _ChapterButtonHolder)
+        {
+            Destroy(child.gameObject);
+        }
+        // Spawn all chapter button
+        foreach (ContentChapterSO contentChapter in _Chapters)
+        {
+            GameObject newChapterButton = Instantiate(_ChapterButtonPrefab, _ChapterButtonHolder);
+            newChapterButton.GetComponent<ChapterButton>().SetChapter(contentChapter);
+        }
+        DeselectChapterButtons();
+        GetComponentInChildren<ChapterButton>().GetComponent<Button>().onClick.Invoke();
+        _RefreshPage();
     }
 
-    private void Update()
+    // Refresh the content on the current page
+    private void _RefreshPage()
     {
-        // Continually display the content of the current page
-        _ContentPageHeader.text = Chapters[_CurrentChapter].Contents[_CurrentPage].Header;
-        _ContentPageImage.sprite = Chapters[_CurrentChapter].Contents[_CurrentPage].Image;
-        _ContentPageDescription.text = Chapters[_CurrentChapter].Contents[_CurrentPage].Description;
+        _ContentPageHeader.text = _CurrentChapterSO.Contents[_CurrentPage].Header;
+        _ContentPageImage.sprite = _CurrentChapterSO.Contents[_CurrentPage].Image;
+        _ContentPageDescription.text = _CurrentChapterSO.Contents[_CurrentPage].Description;
     }
 
     // Add the page of current chapter
@@ -41,46 +64,40 @@ public class ResearchLabManager : MonoBehaviour
         {
             _CurrentPage = 0;
         }
-        else if (_CurrentPage >= Chapters[_CurrentChapter].Contents.Length)
+        else if (_CurrentPage >= _CurrentChapterSO.Contents.Length)
         {
-            _CurrentPage = Chapters[_CurrentChapter].Contents.Length - 1;
+            _CurrentPage = _CurrentChapterSO.Contents.Length - 1;
         }
         _SetPageButtons();
+        _RefreshPage();
     }
 
-    // Change the content chapter
-    public void ChangeChapter(int newChapter)
+    // Set current chapter to display
+    public void SetCurrentChapter(ContentChapterSO newContentChapter)
     {
-        Debug.Log("Change to chapter " + newChapter.ToString());
-        _CurrentChapter = newChapter;
+        _CurrentChapterSO = newContentChapter;
+        Debug.Log("Set current chapter to " + newContentChapter.Header);
         _CurrentPage = 0;
-        _SetChapterButtons();
         _SetPageButtons();
+        _RefreshPage();
     }
 
     // Set change page button to proper color
     private void _SetPageButtons()
     {
-        int maxPage = Chapters[_CurrentChapter].Contents.Length - 1;
+        int maxPage = _CurrentChapterSO.Contents.Length - 1;
         _PreviousPageButton.GetComponent<Image>().color = (_CurrentPage > 0) ? Color.green : Color.white;
         _PreviousPageButton.GetComponent<Button>().interactable = (_CurrentPage > 0) ? true : false;
         _NextPageButton.GetComponent<Image>().color = (_CurrentPage < maxPage) ? Color.green : Color.white;
         _NextPageButton.GetComponent<Button>().interactable = (_CurrentPage < maxPage) ? true : false;
     }
-    // Set chapter button to proper color
-    private void _SetChapterButtons()
+    // Deselect all chapter buttons
+    public void DeselectChapterButtons()
     {
-        Debug.Log("ChapterButtons length = " + _ChapterButtons.Length.ToString());
-        Debug.Log("Current chapter = " + _CurrentChapter.ToString());
-        for (int i = 0; i < _ChapterButtons.Length; i++)
+        _ChapterButtons = _ChapterButtonHolder.GetComponentsInChildren<ChapterButton>();
+        foreach (ChapterButton chapterButton in _ChapterButtons)
         {
-            _ChapterButtons[i].GetComponent<Button>().interactable = (i == _CurrentChapter) ? false : true;
-            if (i == _CurrentChapter)
-            {
-                Debug.Log("Button of Chapter " + i.ToString() + " not interactable");
-                Debug.Log(_ChapterButtons[i].GetComponent<Button>().interactable);
-            }
+            chapterButton.SetIsSelected(false);
         }
-        Debug.Log("Set ChapterButtons");
     }
 }
